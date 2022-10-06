9e9faf3e32e1909af84d623c2d59873c12e2ddbc
Andrey Vasilenko <vasilenko.andrey77@outlook.com>
2022-10-06 12:57:53 +0700

    Imp. base repository

--- SimpleBlog.Data/BlogContext.cs
+++ SimpleBlog.Data/BlogContext.cs
@@ -1,22 +1,15 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SimpleBlog.Model.Entities;
-using System;
-using System.Collections.Generic;
-using System.Data.Common;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace SimpleBlog.Data
+namespace SimpleBlog.Data;
+
+public class BlogContext : DbContext
 {
-    public class BlogContext : DbContext
-    {
-        public DbSet<User> Users { get; set; }
+    public DbSet<User> Users { get; set; }
 
-        public BlogContext(DbContextOptions<BlogContext> options) : base(options)
-        { }
-        
+    public BlogContext(DbContextOptions<BlogContext> options) : base(options)
+    { }
 
-    
-    }
+
+
 }
--- SimpleBlog.Data/Repository/EntityBaseRepository.cs
+++ SimpleBlog.Data/Repository/EntityBaseRepository.cs
@@ -1,92 +1,96 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using SimpleBlog.Data.Abstract;
 using SimpleBlog.Model.Contracts;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace SimpleBlog.Data.Repository
+namespace SimpleBlog.Data.Repository;
+
+public class EntityBaseRepository<T> : IEntityBaseRepository<T>
+    where T : class, IEntityBase, new()
 {
-    public class EntityBaseRepository<T> : IEntityBaseRepository<T>
-        where T : class, IEntityBase, new()
+    private BlogContext _context;
+
+    public EntityBaseRepository(BlogContext context)
     {
-        private BlogContext _context;
+        _context = context;
+    }
 
-        public EntityBaseRepository(BlogContext context)
+    public IEnumerable<T> GetAll()
+    {
+        return _context.Set<T>().AsEnumerable();
+    }
+
+    public IEnumerable<T> AllIncluding(params Expression<Func<T, object>>[] includeProperties)
+    {
+        IQueryable<T> query = _context.Set<T>();
+        foreach (var includeProperty in includeProperties)
         {
-            _context = context;
+            query = query.Include(includeProperty);
+        }
+        return query.AsEnumerable();
+    }
+
+    public int Count()
+    {
+        return _context.Set<T>().Count();
+    }
+
+    public T GetSingle(string id)
+    {
+        return _context.Set<T>().FirstOrDefault(x => x.Id == id);
+    }
+
+    public T GetSingle(Expression<Func<T, bool>> predicate)
+    {
+        return _context.Set<T>().FirstOrDefault(predicate);
+    }
+
+    public T GetSingle(Expression<Func<T, bool>> predicate, params Expression<Func<T, object>>[] includeProperties)
+    {
+        IQueryable<T> query = _context.Set<T>();
+        foreach (var includeProperty in includeProperties)
+        {
+            query = query.Include(includeProperty);
         }
 
-        public IEnumerable<T> GetAll()
+        return query.Where(predicate).FirstOrDefault();
+    }
+
+    public IEnumerable<T> FindBy(Expression<Func<T, bool>> predicate)
+    {
+        return _context.Set<T>().Where(predicate);
+    }
+
+    public void Add(T entity)
+    {
+        EntityEntry dbEntityEntry = _context.Entry<T>(entity);
+        _context.Set<T>().Add(entity);
+    }
+
+    public void Update(T entity)
+    {
+        EntityEntry dbEntityEntry = _context.Entry<T>(entity);
+        dbEntityEntry.State = EntityState.Modified;
+    }
+
+    public void Delete(T entity)
+    {
+        EntityEntry dbEntityEntry = _context.Entry<T>(entity);
+        dbEntityEntry.State = EntityState.Deleted;
+    }
+    public void DeleteWhere(Expression<Func<T, bool>> predicate)
+    {
+        IEnumerable<T> entites = _context.Set<T>().Where(predicate);
+
+        foreach(var entity in entites)
         {
-            return _context.Set<T>().AsEnumerable();
+            _context.Entry<T>(entity).State = EntityState.Deleted;
         }
+    }
 
-        public int Count()
-        {
-            return _context.Set<T>().Count();
-        }
-        public IEnumerable<T> AllIncluding(params Expression<Func<T, object>>[] includeProperties)
-        {
-            IQueryable<T> query = _context.Set<T>().AsQueryable();
-            foreach (var includeProperty in includeProperties)
-            {
-                query = query.Include(includeProperty);
-            }
-            return query.AsEnumerable();
-        }
-
-
-
-        public void Add(T entity)
-        {
-            throw new NotImplementedException();
-        }
-
-
-        public void Commit()
-        {
-            throw new NotImplementedException();
-        }
-
-
-        public void Delete(T entity)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void DeleteWhere(Expression<Func<T, bool>> predicate)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IEnumerable<T> FindBy(Expression<Func<T, bool>> predicate)
-        {
-            throw new NotImplementedException();
-        }
-
-
-        public T GetSingle(string id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public T GetSingle(Expression<Func<T, bool>> predicate)
-        {
-            throw new NotImplementedException();
-        }
-
-        public T GetSingle(Expression<Func<T, bool>> predicate, params Expression<Func<T, object>>[] includeProperties)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Update(T entity)
-        {
-            throw new NotImplementedException();
-        }
+    public virtual void Commit()
+    {
+        _context.SaveChanges();
     }
 }
